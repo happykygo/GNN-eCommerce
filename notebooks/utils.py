32bf846a4982b881c8921d3b2ef149daedb1daa5
happykygo <happykygo@gmail.com>
2023-02-11 21:15:27 -0500

    Introduce save best model mechanism. Fix negative sampling bug.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -223,14 +223,16 @@ def pos_neg_edge_index(train_pos_list_df, n_neg, n_users, n_itm):
         users, pos_items, neg_items
 
     """
-
     u = [[a]*len(b)*n_neg for a, b in zip(train_pos_list_df.user_id_idx, train_pos_list_df.item_id_idx_list)]
-    users = torch.LongTensor(sum(u, []))
+    u = [item for sublist in u for item in sublist]
+    users = torch.LongTensor(u)
     p = train_pos_list_df['item_id_idx_list'].apply(lambda x: x * n_neg).tolist()
-    pos_items = torch.LongTensor(sum(p, []))
+    p = [item for sublist in p for item in sublist]
+    pos_items = torch.LongTensor(p)
     n = [sample_neg(a, n_neg*len(b), n_users, n_itm) for a, b in
          zip(train_pos_list_df.ignor_neg_list, train_pos_list_df.item_id_idx_list)]
-    neg_items = torch.LongTensor(sum(n, []))
+    n = [item for sublist in n for item in sublist]
+    neg_items = torch.LongTensor(n)
 
     return users, pos_items, neg_items
 
@@ -270,7 +272,7 @@ def regularization_loss(init_embed, batch_size, batch_usr, batch_pos, batch_neg,
     return reg_loss * DECAY
 
 
-def train_loop(users, pos_items, neg_items, edge_index, edge_weight, model, optimizer, BATCH_SIZE, device):
+def train_loop(users, pos_items, neg_items, edge_index, edge_weight, model, optimizer, BATCH_SIZE, log_interval=10):
     bpr_loss_batch_list = []
     reg_loss_batch_list = []
     final_loss_batch_list = []
@@ -278,6 +280,8 @@ def train_loop(users, pos_items, neg_items, edge_index, edge_weight, model, opti
     idx = list(range(len(users)))
     random.shuffle(idx)
     loader = DataLoader(idx, batch_size=BATCH_SIZE, shuffle=True)
+
+    dataset_size = len(loader.dataset)
 
     model.train()
     for batch_num, batch in enumerate(loader):
@@ -302,6 +306,11 @@ def train_loop(users, pos_items, neg_items, edge_index, edge_weight, model, opti
         bpr_loss_batch_list.append(bpr_loss.item())
         reg_loss_batch_list.append(reg_loss.item())
         final_loss_batch_list.append(loss.item())
+
+        if batch_num % log_interval == 0:
+            bpr, reg, loss = bpr_loss.item(), reg_loss.item(), loss.item()
+            current = batch_num * len(batch)
+            print(f"bpr/reg/total loss: {bpr:>7f} {reg:>7f} {loss:>7f}  [{current:>5d}/{dataset_size} users]")
 
     bpr_loss = round(np.mean(bpr_loss_batch_list), 4)
     reg_loss = round(np.mean(reg_loss_batch_list), 4)
@@ -367,9 +376,8 @@ def evaluation(model, n_users, n_items, edge_index, edge_weight, test_pos_list_d
     return precision, recall
 
 
-def train_and_evl(n_users, n_items, n_neg, train_df, train_pos_list_df, test_pos_list_df, model, optimizer, device,
-                  EPOCHS=50, BATCH_SIZE=1024, K=20, DECAY=0.0001):  # test_u_i_matrix,
-    edge_index, edge_weight = df_to_graph(train_df, True)
+def train_and_evl(n_users, n_items, n_neg, edge_index, edge_weight, train_pos_list_df, test_pos_list_df, model, optimizer, device,
+                  EPOCHS=50, BATCH_SIZE=1024, K=20, DECAY=0.0001, checkpoint_dir="", log_interval=10):  # test_u_i_matrix,
 
     # test_u_i_matrix = test_u_i_matrix.to(device)
     edge_index = edge_index.to(device)
@@ -382,24 +390,32 @@ def train_and_evl(n_users, n_items, n_neg, train_df, train_pos_list_df, test_pos
     recall_epoch_list = []
     precision_epoch_list = []
 
-    print('bpr_loss | reg_loss | final_loss | precision | recall')
+    best_recall = 0.0
+
     for epoch in tqdm(range(EPOCHS)):
+        print(f"------- Epoch {epoch} -------------------------------")
         users, pos_items, neg_items = pos_neg_edge_index(train_pos_list_df, n_neg, n_users, n_items)
         users = users.to(device)
         pos_items = pos_items.to(device)
         neg_items = neg_items.to(device)
 
         bpr_loss, reg_loss, final_loss = train_loop(users, pos_items, neg_items, edge_index, edge_weight, model,
-                                                    optimizer, BATCH_SIZE, device)
+                                                    optimizer, BATCH_SIZE, log_interval)
 
         precision, recall = evaluation(model, n_users, n_items, edge_index, edge_weight, test_pos_list_df, K)
 
-        print(bpr_loss, reg_loss, final_loss, precision, recall)
         bpr_loss_epoch_list.append(bpr_loss)
         reg_loss_epoch_list.append(reg_loss)
         final_loss_epoch_list.append(final_loss)
         recall_epoch_list.append(recall)
         precision_epoch_list.append(precision)
+
+        print(f"Epoch {epoch} Precision: {precision:>0.4f}, Recall: {recall:>0.4f}")
+
+        # save the best model
+        if recall > best_recall:
+            best_recall = recall
+            save_model(epoch, model, optimizer, precision, recall, checkpoint_dir + "/LightGCN_best.pt")
 
     return (
         bpr_loss_epoch_list,
@@ -407,3 +423,18 @@ def train_and_evl(n_users, n_items, n_neg, train_df, train_pos_list_df, test_pos
         final_loss_epoch_list,
         recall_epoch_list,
         precision_epoch_list)
+
+
+def save_model(epochs, model, optimizer, precision, recall, path):
+    """
+    Function to save the trained model to disk.
+    """
+    torch.save({
+                'epoch': epochs,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'precision': precision,
+                'recall': recall
+                }, path)
+
+    print(f"Model {path} saved at epoch {epochs}")
