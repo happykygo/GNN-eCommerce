40b370083ccede0cc9be332f26a4f10f453c7b39
happykygo <happykygo@gmail.com>
2023-02-20 16:58:13 -0500

    torchserve custom model handler

---
+++ torchserve/lightgcn_handler.py
@@ -0,0 +1,105 @@
+import torch
+from ts.torch_handler.base_handler import BaseHandler
+import pandas as pd
+import os
+
+
+class LightGCNHandler(BaseHandler):
+    """
+    A custom model handler implementation.
+    """
+    def initialize(self, context):
+        """
+        Invoke by torchserve for loading a model
+        :param context: context contains model server system properties
+        :return:
+        """
+
+        #  load the model
+        self.manifest = context.manifest
+
+        properties = context.system_properties
+        model_dir = properties.get("model_dir")
+        self.device = torch.device("cuda:" + str(properties.get("gpu_id")) if torch.cuda.is_available() else "cpu")
+
+        # Read model serialize/pt file
+        serialized_file = self.manifest['model']['ameri']
+        model_pt_path = os.path.join(model_dir, serialized_file)
+        if not os.path.isfile(model_pt_path):
+            raise RuntimeError("Missing the model.pt file")
+
+        self.model = torch.jit.load(model_pt_path)
+
+        train_df_path = os.path.join(model_dir, 'processed_train.csv')
+        train_df = pd.read_csv(train_df_path)
+        self.n_users = train_df['user_id_idx'].nunique()
+        self.n_items = train_df['item_id_idx'].nunique()
+        train_df['item_id_idx'] = train_df['item_id_idx'] - self.n_users
+        self.i_m_matrix = self.interact_matrix(train_df, self.n_users, self.n_items)  # keep in cpu
+        self.edge_index, self.edge_weight = self.df_to_graph(train_df, True)
+
+        self.edge_index = self.edge_index.to(self.device)
+        self.edge_weight = self.edge_weight.to(self.device)
+        self.i_m_matrix = self.i_m_matrix.to(self.device)
+        self.model.to(self.device)
+
+        self.initialized = True
+
+    def inference(self, data, *args, **kwargs):
+        """The Inference Request is made through this function and the user
+        needs to override the inference function to customize it.
+
+        Args:
+            data (torch tensor): The data is in the form of Torch Tensor
+                                 whose shape should match that of the
+                                  Model Input shape.
+
+        Returns:
+            (Torch Tensor): The predicted response from the model is returned
+                            in this function.
+        """
+        with torch.no_grad():
+            marshalled_data = data.to(self.device)
+            interactions_t = torch.index_select(self.i_m_matrix, 0, marshalled_data).to_dense()
+
+            k = 20
+            topK_df = self.model.recommendK(self.edge_index, self.edge_weight, self.n_users, self.n_items,
+                                            interactions_t, marshalled_data, k)
+
+            topK = topK_df['top_rlvnt_itm']
+
+        return topK
+
+    def df_to_graph(self, train_df, weight):
+        r"""Convert dataset to bipartite graph_edge_index
+        Args:
+            :param train_df: (Tensor) Raw train_df
+            :param weight: Graph contains weight or not
+        Returns:
+        """
+        u_t = torch.LongTensor(train_df['user_id_idx'].values)
+        i_t = torch.LongTensor(train_df['item_id_idx'].values)
+        graph_edge_index = torch.stack((
+            torch.cat([u_t, i_t]),
+            torch.cat([i_t, u_t])
+        ))
+
+        if weight:
+            w_t = torch.FloatTensor(train_df['weight'].values)
+            edge_weights = torch.cat([w_t, w_t])
+            return graph_edge_index, edge_weights
+
+        return graph_edge_index
+
+    def interact_matrix(self, train_df, n_users, n_items):
+        r"""
+        create sparse tensor of all user-item interactions
+        """
+        df = train_df.loc[train_df['weight'] == 1.0]
+        i = torch.stack((
+            torch.LongTensor(df['user_id_idx'].values),
+            torch.LongTensor(df['item_id_idx'].values)
+        ))
+        v = torch.ones((len(df)), dtype=torch.float32)
+        interactions_t = torch.sparse.FloatTensor(i, v, (n_users, n_items))
+        return interactions_t
